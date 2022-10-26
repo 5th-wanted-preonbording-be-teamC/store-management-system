002b5c0bc5f8aed9de086122c353a1b4ed12297b
HoJin9622 <kiss0104040@gmail.com>
2022-10-26 10:54:14 +0900

    FEAT: view 및 url 설정

--- products/views.py
+++ products/views.py
@@ -1,3 +1,28 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.exceptions import NotFound
+from .models import Product
 
-# Create your views here.
+
+class Products(APIView):
+    def get(self, request):
+        pass
+
+    def post(self, request):
+        pass
+
+
+class ProductDetail(APIView):
+    def get_object(self, pk):
+        try:
+            return Product.objects.get(pk=pk)
+        except Product.DoesNotExist:
+            raise NotFound
+
+    def get(self, request, pk):
+        pass
+
+    def patch(self, request, pk):
+        pass
+
+    def delete(self, request, pk):
+        pass
