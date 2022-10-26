3c578ad62db4fe13ed9ae3fde75795da27ec89b4
HoJin9622 <kiss0104040@gmail.com>
2022-10-26 13:20:05 +0900

    FEAT: 상품 삭제 api 생성

--- products/views.py
+++ products/views.py
@@ -1,7 +1,7 @@
 from rest_framework.views import APIView
 from rest_framework.exceptions import NotFound
 from rest_framework.response import Response
-from rest_framework.status import HTTP_400_BAD_REQUEST
+from rest_framework.status import HTTP_400_BAD_REQUEST, HTTP_204_NO_CONTENT
 from .models import Product
 from .serializers import ProductListSerializer, ProductDetailSerializer
 from .permissions import IsAdminOrReadOnly
@@ -16,6 +16,7 @@ class Products(APIView):
         상품 목록
         GET /api/v1/products/
         """
+
         products = Product.objects.all()
         serializer = ProductListSerializer(products, many=True)
 
@@ -26,6 +27,7 @@ class Products(APIView):
         상품 생성
         POST /api/v1/products/
         """
+
         serializer = ProductDetailSerializer(data=request.data)
         if serializer.is_valid():
             product = serializer.save()
@@ -43,6 +45,11 @@ class ProductDetail(APIView):
             raise NotFound
 
     def get(self, request, pk):
+        """
+        상품 상세
+        GET /api/v1/products/{pk}/
+        """
+
         product = self.get_object(pk)
         serializer = ProductDetailSerializer(product)
         return Response(serializer.data)
@@ -51,4 +58,11 @@ class ProductDetail(APIView):
         pass
 
     def delete(self, request, pk):
-        pass
+        """
+        상품 삭제
+        DELETE /api/v1/products/{pk}/
+        """
+
+        product = self.get_object(pk)
+        product.delete()
+        return Response(status=HTTP_204_NO_CONTENT)
