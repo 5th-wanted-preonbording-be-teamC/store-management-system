062d8cb1b19bec5321100cc1cd76c2807245e804
xxddongxx <hyeon2187@gmail.com>
2022-10-27 09:03:22 +0900

    FEAT 회원 정보 수정 api

--- users/views.py
+++ users/views.py
@@ -41,3 +41,15 @@ class UserDetailAPIView(APIView):
         user = self.get_object(pk=pk)
         serializer = serializers.UserSerializer(user)
         return Response(serializer.data, status=status.HTTP_200_OK)
+
+    def put(self, request, pk):
+        """
+        회원 정보 수정
+        PUT /api/v1/users/{pk}
+        """
+        user = self.get_object(pk=pk)
+        serializer = serializers.UserUpdateSerializer(user, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
