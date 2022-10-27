99e5bf27217751cd69668e53606721fc0cc27dd2
ChanHaeng Lee <2chanhaeng@gmail.com>
2022-10-27 11:37:33 +0900

    FEAT: Payment List Serializer 생성
    여러 Payment를 가져올 때 사용하는 Serializer인 PaymentListSerializer를 분리하였습니다. 공통된 필드와 개별 필드, 리스트 필드를 분리해서 추후 변경이 용이하도록 만들었습니다.

--- payments/serializers.py
+++ payments/serializers.py
@@ -1,6 +1,23 @@
 from rest_framework import serializers
 from .models import Payment
 
+common_fields = (
+    "id",
+    "product",
+    "user",
+    "price",
+    "delivery_fee",
+    "amount",
+    "payment_method",
+    "delivery_address",
+    "successed_at",
+    "canceled_at",
+    "deliveried_at",
+    "created_at",
+    "updated_at",
+) # 공통 필드
+indivisual_fields = tuple() # 개별 필드
+list_fields = tuple() # 리스트 필드
 
 class PaymentSerializer(serializers.ModelSerializer):
     """
@@ -8,19 +25,15 @@ class PaymentSerializer(serializers.ModelSerializer):
     """
     class Meta:
         model = Payment
-        fields = (
-            "id",
-            "product",
-            "user",
-            "price",
-            "delivery_fee",
-            "amount",
-            "payment_method",
-            "delivery_address",
-            "successed_at",
-            "canceled_at",
-            "deliveried_at",
-            "created_at",
-            "updated_at",
-        )
+        fields = common_fields + indivisual_fields
 
+
+
+class PaymentListSerializer(serializers.ModelSerializer):
+    """
+    결제내역 List Serializer
+    결제내역 Serializer와 동일하나, 추후 필요시 변경할 수 있도록 분리
+    """
+    class Meta:
+        model = Payment
+        fields = common_fields + list_fields
