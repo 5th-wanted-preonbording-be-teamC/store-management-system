25af8a165d6cb10f433f40381f71bba606c80155
HoJin9622 <kiss0104040@gmail.com>
2022-10-28 01:16:53 +0900

    FIX: order permission 버그 수정

--- orders/permissions.py
+++ orders/permissions.py
@@ -4,5 +4,5 @@ from rest_framework.permissions import BasePermission
 class OrderPermission(BasePermission):
     def has_permission(self, request, view):
         if request.method == "GET":
-            return request.user and request.user.is_staff
-        return True
+            return bool(request.user and request.user.is_staff)
+        return bool(request.user and request.user.is_authenticated)
