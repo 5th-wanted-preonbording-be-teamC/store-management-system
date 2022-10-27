4e20a63d343633e7f2ebdefcb3de219e18852cce
ChanHaeng Lee <2chanhaeng@gmail.com>
2022-10-27 13:52:19 +0900

    FIX: SECRET_KEY .env로 이동
    보안을 위해 SECRET_KEY를 환경 변수로 지정하였습니다.

--- config/settings.py
+++ config/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
+from os import environ
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -20,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-2m$tdh!mg@zhkvhn=62%5#vqejdkd6xw8cpw4p)i*2j4@fp$t-"
+SECRET_KEY = environ["SECRET_KEY"]
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
