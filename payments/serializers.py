b5fbbb5fa0a05d2280c1aefbf63de7f98d87fcda
ChanHaeng Lee <2chanhaeng@gmail.com>
2022-10-28 12:41:13 +0900

    STYLE: Type hint 적용

--- payments/serializers.py
+++ payments/serializers.py
@@ -1,7 +1,10 @@
+from typing import Literal, Tuple, Dict, Any, Callable, Optional, Final, Type
 from rest_framework import serializers
 from .models import Payment
 
-common_fields = (
+AttrsType = Dict[str, Optional[str | Any]]
+ValidateType = Callable[[Any, AttrsType], AttrsType]
+common_fields: Final = (
     "id",
     "product",
     "user",
@@ -17,8 +20,8 @@ common_fields = (
     "created_at",
     "updated_at",
 )  # 공통 필드
-indivisual_fields = tuple()  # 개별 필드
-list_fields = tuple()  # 리스트 필드
+indivisual_fields: Final = tuple()  # 개별 필드
+list_fields: Final = tuple()  # 리스트 필드
 
 
 class PaymentSerializer(serializers.ModelSerializer):
@@ -27,8 +30,8 @@ class PaymentSerializer(serializers.ModelSerializer):
     """
 
     class Meta:
-        model = Payment
-        fields = common_fields + indivisual_fields
+        model: Final = Payment
+        fields: Final = common_fields + indivisual_fields
 
 
 class PaymentListSerializer(serializers.ModelSerializer):
@@ -38,8 +41,8 @@ class PaymentListSerializer(serializers.ModelSerializer):
     """
 
     class Meta:
-        model = Payment
-        fields = common_fields + list_fields
+        model: Final = Payment
+        fields: Final = common_fields + list_fields
 
 
 class PaymentUpdateCommonSerializer(serializers.ModelSerializer):
@@ -47,18 +50,19 @@ class PaymentUpdateCommonSerializer(serializers.ModelSerializer):
     결제내역 Update를 위한 추상적 Serializer
     """
 
-    essetial_fields = ""
-    compare_fields = ""
+    essetial_fields: str = ""
+    compare_fields: str = ""
 
-    def additional_validate(validate):
+    def additional_validate(validate: ValidateType) -> ValidateType:
         # 취소 등 추가 검사가 필요한 경우, 이곳에서 추가 검사
-        def wrapper(self, attrs):
+        def wrapper(self, attrs: AttrsType) -> AttrsType:
+
             return validate(self, attrs)
 
         return wrapper
 
     @additional_validate
-    def validate(self, attrs):
+    def validate(self, attrs: AttrsType) -> AttrsType:
         # 필수 필드 검사
         if attrs.get(self.essetial_fields, None) is None:
             raise serializers.ValidationError(f"{self.essetial_fields}가 없습니다.")
@@ -76,12 +80,12 @@ class PaymentSuccessSerializer(PaymentUpdateCommonSerializer):
     결제 성공 Serializer
     """
 
-    essetial_fields = "successed_at"
-    compare_fields = "created_at"
+    essetial_fields: Final = "successed_at"
+    compare_fields: Final = "created_at"
 
     class Meta:
-        model = Payment
-        fields = ("successed_at",)
+        model: Final = Payment
+        fields: Final = ("successed_at",)
 
 
 class PaymentCancelSerializer(PaymentUpdateCommonSerializer):
@@ -89,11 +93,11 @@ class PaymentCancelSerializer(PaymentUpdateCommonSerializer):
     결제 취소 Serializer
     """
 
-    essetial_fields = "canceled_at"
-    compare_fields = "successed_at"
+    essetial_fields: Final = "canceled_at"
+    compare_fields: Final = "successed_at"
 
-    def additional_validate(validate):
-        def wrapper(self, attrs):
+    def additional_validate(validate: ValidateType) -> ValidateType:
+        def wrapper(self, attrs: AttrsType) -> AttrsType:
             if attrs.get("shiped_at", None) is not None:
                 # 배송중인 경우 취소 불가
                 raise serializers.ValidationError("배송이 시작된 상품은 취소할 수 없습니다.")
@@ -102,8 +106,8 @@ class PaymentCancelSerializer(PaymentUpdateCommonSerializer):
         return wrapper
 
     class Meta:
-        model = Payment
-        fields = ("canceled_at",)
+        model: Final = Payment
+        fields: Final = ("canceled_at",)
 
 
 class PaymentShipSerializer(PaymentUpdateCommonSerializer):
@@ -111,11 +115,11 @@ class PaymentShipSerializer(PaymentUpdateCommonSerializer):
     배송 시작 Serializer
     """
 
-    essetial_fields = "shiped_at"
-    compare_fields = "successed_at"
+    essetial_fields: Final = "shiped_at"
+    compare_fields: Final = "successed_at"
 
-    def additional_validate(validate):
-        def wrapper(self, attrs):
+    def additional_validate(validate: ValidateType) -> ValidateType:
+        def wrapper(self, attrs: AttrsType) -> AttrsType:
             if attrs.get("canceled_at", None) is not None:
                 # 이미 취소된 경우 배송 불가
                 raise serializers.ValidationError("이미 취소된 상품은 배송할 수 없습니다.")
@@ -124,8 +128,8 @@ class PaymentShipSerializer(PaymentUpdateCommonSerializer):
         return wrapper
 
     class Meta:
-        model = Payment
-        fields = ("shiped_at",)
+        model: Final = Payment
+        fields: Final = ("shiped_at",)
 
 
 class PaymentDeliverySerializer(PaymentUpdateCommonSerializer):
@@ -133,12 +137,12 @@ class PaymentDeliverySerializer(PaymentUpdateCommonSerializer):
     배송 시작 Serializer
     """
 
-    essetial_fields = "deliveried_at"
-    compare_fields = "shiped_at"
+    essetial_fields: Final = "deliveried_at"
+    compare_fields: Final = "shiped_at"
 
     class Meta:
-        model = Payment
-        fields = ("deliveried_at",)
+        model: Final = Payment
+        fields: Final = ("deliveried_at",)
 
 
 class PaymentDeliveryAddressSerializer(serializers.ModelSerializer):
@@ -146,12 +150,12 @@ class PaymentDeliveryAddressSerializer(serializers.ModelSerializer):
     배송지 변경 Serializer
     """
 
-    def validate(self, attrs):
+    def validate(self, attrs: AttrsType) -> AttrsType:
         if attrs.get("shiped_at") is not None:
             # 배송이 시작된 경우 배송지 변경 불가
             raise serializers.ValidationError("배송이 시작된 상품은 배송지를 변경할 수 없습니다.")
         return attrs
 
     class Meta:
-        model = Payment
-        fields = ("delivery_address",)
+        model: Final = Payment
+        fields: Final = ("delivery_address",)
